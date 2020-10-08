ca755ff8f55851db066b503e5c200e4f1ee331bb
sshingh67 <singhsharandeep676@gmail.com>
2020-10-08 14:52:35 -0700

    component for dispalying each movie object

---
+++ src/components/common/tableBody.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import _ from "lodash";
+
+class TableBody extends Component {
+  renderCell = (item, column) => {
+    if (column.content) return column.content(item);
+
+    return _.get(item, column.path);
+  };
+
+  createKey = (item, column, valueProperty) => {
+    return item[valueProperty] + (column.path || column.key);
+  };
+
+  render() {
+    const { data, columns, valueProperty } = this.props;
+    return (
+      <tbody>
+        {data.map((item) => (
+          <tr key={item[valueProperty]}>
+            {columns.map((column) => (
+              <td key={this.createKey(item, column, valueProperty)}>
+                {this.renderCell(item, column)}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
+}
+
+TableBody.defaultProps = {
+  valueProperty: "_id",
+};
+
+export default TableBody;
