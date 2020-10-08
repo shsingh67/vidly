ae1813f4191739ff2a44ccff20b6dfdf8a003aad
sshingh67 <singhsharandeep676@gmail.com>
2020-10-08 14:50:26 -0700

    like componenet for liking movies

---
+++ src/components/common/like.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const Like = ({ liked, onClick }) => {
+  let classes = "fa fa-heart";
+  if (!liked) classes += "-o";
+  return (
+    <i
+      onClick={onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    ></i>
+  );
+};
+
+export default Like;
