155aaf8a3aa1b4aab490606d02eba822df20f9a7
sshingh67 <singhsharandeep676@gmail.com>
2020-10-26 14:43:24 -0700

    hid create new movie button for users not logged in

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -104,6 +104,8 @@ class Movies extends Component {
       searchQuery,
     } = this.state;
 
+    const { user } = this.props;
+
     if (count === 0) {
       return <p>There are no movies in the database.</p>;
     }
@@ -120,13 +122,15 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <Link
-            to="/movies/new"
-            className="btn btn-primary"
-            style={{ marginBottom: 20 }}
-          >
-            New Movie
-          </Link>
+          {user && (
+            <Link
+              to="/movies/new"
+              className="btn btn-primary"
+              style={{ marginBottom: 20 }}
+            >
+              New Movie
+            </Link>
+          )}
           <p>Showing {totalCount} movies in the database.</p>
           <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
